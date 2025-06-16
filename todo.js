eff6ad25a4f902181d8433c27f117ec2be09a519
ysktbb <110451941+ysktbb@users.noreply.github.com>
2025-06-16 22:50:10 +0900

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,7 +1,13 @@
 // 当日を保持するキー
-let todayKey = "today";
+const TODAY_KEY = "today";
 // チェック状態を保持するキー
-let checkStatusKey = "checkStatus";
+const CHECK_STATUS_KEY = "checkStatus";
+// 完了済み
+const COMPLETED = "済";
+// 不要
+const NOTHING = "無";
+// TODO一覧
+let todoList = ["1_0", "1_1", "1_2", "1_3", "1_4", "2_0", "3_0", "3_1", "3_2", "3_3"];
 
 window.onload = function() {
   if (localStorage) {
@@ -10,97 +16,80 @@ window.onload = function() {
     let ymd = date.getFullYear() + ("00" + (date.getMonth() + 1)).slice(-2) + ("00" + date.getDate()).slice(-2);
     
     // 本日 = 当日 の判定
-    if (ymd == localStorage.getItem(todayKey)) {
+    if (ymd == localStorage.getItem(TODAY_KEY)) {
       // 同じ、チェック状態を取得
-      let checkStatus = JSON.parse(localStorage.getItem(checkStatusKey));
-      c(checkStatus);
-      if (checkStatus.todo1_0 != "") {
-        document.getElementById("todo1_0").value = checkStatus.todo1_0;
-        document.getElementById("task1_0").classList.add("completed");
-        document.getElementById("time1_0").innerText = checkStatus.todo1_0;
-      }
-      if (checkStatus.todo1_1 != "") {
-        document.getElementById("todo1_1").value = checkStatus.todo1_1;
-        document.getElementById("task1_1").classList.add("completed");
-        document.getElementById("time1_1").innerText = checkStatus.todo1_1;
-      }
-      if (checkStatus.todo1_2 != "") {
-        document.getElementById("todo1_2").value = checkStatus.todo1_2;
-        document.getElementById("task1_2").classList.add("completed");
-        document.getElementById("time1_2").innerText = checkStatus.todo1_2;
-      }
-      if (checkStatus.todo1_3 != "") {
-        document.getElementById("todo1_3").value = checkStatus.todo1_3;
-        document.getElementById("task1_3").classList.add("completed");
-        document.getElementById("time1_3").innerText = checkStatus.todo1_3;
-      }
-      if (checkStatus.todo1_4 != "") {
-        document.getElementById("todo1_4").value = checkStatus.todo1_4;
-        document.getElementById("task1_4").classList.add("completed");
-        document.getElementById("time1_4").innerText = checkStatus.todo1_4;
-      }                                 
-      if (checkStatus.todo2_0 != "") {
-        document.getElementById("todo2_0").value = checkStatus.todo2_0;
-        document.getElementById("task2_0").classList.add("completed");
-        document.getElementById("time2_0").innerText = checkStatus.todo2_0;
-      }
-      if (checkStatus.todo3_0 != "") {
-        document.getElementById("todo3_0").value = checkStatus.todo3_0;
-        document.getElementById("task3_0").classList.add("completed");
-        document.getElementById("time3_0").innerText = checkStatus.todo3_0;
-      }
-      if (checkStatus.todo3_1 != "") {
-        document.getElementById("todo3_1").value = checkStatus.todo3_1;
-        document.getElementById("task3_1").classList.add("completed");
-        document.getElementById("time3_1").innerText = checkStatus.todo3_1;
-      }
-      if (checkStatus.todo3_2 != "") {
-        document.getElementById("todo3_2").value = checkStatus.todo3_2;
-        document.getElementById("task3_2").classList.add("completed");
-        document.getElementById("time3_2").innerText = checkStatus.todo3_2;
-      }
-      if (checkStatus.todo3_3 != "") {
-        document.getElementById("todo3_3").value = checkStatus.todo3_3;
-        document.getElementById("task3_3").classList.add("completed");
-        document.getElementById("time3_3").innerText = checkStatus.todo3_3;
+      let checkStatus = JSON.parse(localStorage.getItem(CHECK_STATUS_KEY));
+      for (let todo of todoList) {
+        if (checkStatus[todo][0] != "") {
+          let trObj = document.getElementById(todo);
+          if (checkStatus[todo][0] == COMPLETED) {
+            trObj.children[0].innerHTML = "<div class='done'>" + checkStatus[todo][0] + "</div>";
+          } else {
+            trObj.children[0].innerHTML = "<div>" + checkStatus[todo][0] + "</div>";
+          }
+          trObj.children[1].classList.add("completed");
+          trObj.children[3].innerText = checkStatus[todo][1];
+        }
       }
     } else {
       // 同じではない、チェック状態をクリア
-      localStorage.removeItem(checkStatusKey);
+      localStorage.removeItem(CHECK_STATUS_KEY);
       // 本日を設定
-      localStorage.setItem(todayKey, ymd);
+      localStorage.setItem(TODAY_KEY, ymd);
     }
   }
 };
 
-function changeStatus(id) {
-  if (document.getElementById("task" + id).classList.contains("completed")) {
-    document.getElementById("task" + id).classList.remove("completed");
-    document.getElementById("todo" + id).value = "";
-    document.getElementById("time" + id).innerText = "";
-  } else {
-    document.getElementById("task" + id).classList.add("completed");
-    document.getElementById("todo" + id).value = ("00" + new Date().getHours()).slice(-2) + ":" + ("00" + new Date().getMinutes()).slice(-2);
-    document.getElementById("time" + id).innerText = ("00" + new Date().getHours()).slice(-2) + ":" + ("00" + new Date().getMinutes()).slice(-2);
+function changeStatus(obj) {
+  let trObj = obj.parentNode;
+  if (obj.classList.contains("task")) {
+    // タスクをクリック
+    if (trObj.children[0].innerText == COMPLETED) {
+      // 完了済み ⇒ 未完了
+      trObj.children[0].innerText = "";
+      trObj.children[1].classList.remove("completed");
+      trObj.children[3].innerText = "";
+    } else if (trObj.children[0].innerText == NOTHING) {
+      // 何もしない
+    } else {
+      // 未完了 ⇒ 完了済み
+      trObj.children[0].innerHTML = "<div class='done'>" + COMPLETED + "</div>";
+      trObj.children[1].classList.add("completed");
+      trObj.children[3].innerText = ("00" + new Date().getHours()).slice(-2) + ":" + ("00" + new Date().getMinutes()).slice(-2);
+    }
+  } else if (obj.classList.contains("none")) {
+    // 不要をクリック
+    if (trObj.children[0].innerText == COMPLETED) {
+      // 何もしない
+    } else if (trObj.children[0].innerText == NOTHING) {
+      // 不要 ⇒ 未完了
+      trObj.children[0].innerText = "";
+      trObj.children[1].classList.remove("completed");
+      trObj.children[3].innerText = "";
+    } else {
+      // 未完了 ⇒ 不要
+      trObj.children[0].innerHTML = "<div>" + NOTHING + "</div>";
+      trObj.children[1].classList.add("completed");
+      trObj.children[3].innerText = ("00" + new Date().getHours()).slice(-2) + ":" + ("00" + new Date().getMinutes()).slice(-2);
+    }
   }
   setLocalStorage();
 }
 
 function setLocalStorage() {
-c(document.getElementById("todo1_0").value);
   let data = {
-    "todo1_0": document.getElementById("todo1_0").value,
-    "todo1_1": document.getElementById("todo1_1").value,
-    "todo1_2": document.getElementById("todo1_2").value,
-    "todo1_3": document.getElementById("todo1_3").value,
-    "todo1_4": document.getElementById("todo1_4").value,
-    "todo2_0": document.getElementById("todo2_0").value,
-    "todo3_0": document.getElementById("todo3_0").value,
-    "todo3_1": document.getElementById("todo3_1").value,
-    "todo3_2": document.getElementById("todo3_2").value,
-    "todo3_3": document.getElementById("todo3_3").value
+    "1_0": [document.getElementById("1_0").children[0].innerText, document.getElementById("1_0").children[3].innerText],
+    "1_1": [document.getElementById("1_1").children[0].innerText, document.getElementById("1_1").children[3].innerText],
+    "1_2": [document.getElementById("1_2").children[0].innerText, document.getElementById("1_2").children[3].innerText],
+    "1_3": [document.getElementById("1_3").children[0].innerText, document.getElementById("1_3").children[3].innerText],
+    "1_4": [document.getElementById("1_4").children[0].innerText, document.getElementById("1_4").children[3].innerText],
+    "2_0": [document.getElementById("2_0").children[0].innerText, document.getElementById("2_0").children[3].innerText],
+    "3_0": [document.getElementById("3_0").children[0].innerText, document.getElementById("3_0").children[3].innerText],
+    "3_1": [document.getElementById("3_1").children[0].innerText, document.getElementById("3_1").children[3].innerText],
+    "3_2": [document.getElementById("3_2").children[0].innerText, document.getElementById("3_2").children[3].innerText],
+    "3_3": [document.getElementById("3_3").children[0].innerText, document.getElementById("3_3").children[3].innerText],
   };
-  localStorage.setItem(checkStatusKey, JSON.stringify(data));
+  localStorage.setItem(CHECK_STATUS_KEY, JSON.stringify(data));
 }
 
 function c(msg) {
